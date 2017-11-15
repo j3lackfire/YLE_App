2959a6b3c8cc12b56f788242f1b24d0459a18163
j3lackfire <j3llackfire@gmail.com>
2017-11-15 13:09:04 +0200

    Create YLE connector with simple GET request test

---
+++ Assets/Scripts/YleConnector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class YleConnector : MonoBehaviour {
+
+    private void Awake()
+    {
+
+    }
+	
+	private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(TestFunction());
+        }
+    }
+
+    private IEnumerator TestFunction()
+    {
+        Debug.Log("<color=cyan> Test function </color>");
+        UnityWebRequest www = UnityWebRequest.Get("https://external.api.yle.fi/v1/programs/items.json?" + GlobalValues.GetPersonalKey());
+        Debug.Log(Time.timeSinceLevelLoad);
+        yield return www.SendWebRequest();
+        Debug.Log(Time.timeSinceLevelLoad);
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+
+    }
+}
